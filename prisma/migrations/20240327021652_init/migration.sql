de9341e40f3ce3ad5c6964d64f2b7eb1ad690eca
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2024-03-26 22:20:37 -0400

    Change to mysql

--- prisma/migrations/20240327021652_init/migration.sql
+++ prisma/migrations/20240327021652_init/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
 CREATE TABLE "PitList" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "signed" TEXT NOT NULL,
     "event" TEXT NOT NULL,
     "match" TEXT NOT NULL,
     "checks" TEXT NOT NULL,
-    "submitted_at" DATETIME
+    "submitted_at" TIMESTAMP(3),
+
+    CONSTRAINT "PitList_pkey" PRIMARY KEY ("id")
 );
